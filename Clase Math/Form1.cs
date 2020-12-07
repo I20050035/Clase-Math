fd95ec70a75eb57102a5c7798ee31c4aada387fb
I20050035 <70992098+I20050035@users.noreply.github.com>
2020-12-07 16:29:17 -0600

    Add files via upload

---
+++ Clase Math/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clase_Math
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = null;
+            textBox2.Text = null;
+            textBox3.Text = null;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double cant1, cant2, resultado;
+            cant1 = Convert.ToDouble(textBox1.Text);
+            cant2 = Convert.ToDouble(textBox2.Text);
+            resultado = Math.Pow(cant1, cant2);
+            textBox3.Text = Convert.ToString(resultado);
+        }
+    }
+}
